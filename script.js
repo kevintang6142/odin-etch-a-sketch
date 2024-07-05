91ce9db1ea6c85aee362bbe8bcdaf886e664b7f1
kevintang6142 <kevintang6142@gmail.com>
2024-07-05 13:57:32 -0500

    Add display showing resolution and change  button text

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const body = document.querySelector("body");
 
 const button = document.querySelector("button");
+const p = document.querySelector("p");
 button.addEventListener("click", e => {
     let gridSize;
     do {
-        gridSize = prompt("Enter a valid sketch resolution (1-100): ");
+        gridSize = prompt("Enter a valid resolution (1-100): ");
     } while (isNaN(gridSize) || gridSize != parseInt(gridSize) || gridSize < 1 || gridSize > 100)
     createGrid(gridSize);
 })
@@ -15,6 +16,7 @@ grid.id = "grid";
 createGrid(16);
 
 function createGrid(gridSize){
+    p.textContent = `Current Resolution: ${gridSize} x ${gridSize}`;
     grid.innerText = "";
     const squareSize = 500 / gridSize;
     for (let i = 0; i < gridSize ** 2; i++) {
